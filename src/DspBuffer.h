eb7ee1c4dcdf41e91c2922ad308cfe73c613d70f
Jim Clay <jim@whamcitylights.com>
2014-05-26 00:22:14 -0400

    Added some DspBuffer creator methods and unit tests.

--- src/DspBuffer.h
+++ src/DspBuffer.h
@@ -1,8 +1,43 @@
+#ifndef SMART_DSP_DSPBUFFER
+#define SMART_DSP_DSPBUFFER
+
 
 #include <vector>
 
+const int DEFAULT_BUF_LEN = 0;
+enum DomainType {TIME_DOMAIN, FREQUENCY_DOMAIN};
+
+
+#define VECTOR_TO_ARRAY(x)      (&x[0])
+
+
 template <class T>
 class DspBuffer {
+
+private:
+    void initSize(int size, DomainType dataDomain = TIME_DOMAIN) {buf = std::vector<T>(size); domain = dataDomain;}
+    template <class U>
+    void initArray(U *array, int arrayLen, DomainType dataDomain = TIME_DOMAIN);
+    
 public:
 	std::vector<T> buf;
+    DomainType domain;
+    
+    DspBuffer<T>(int size = DEFAULT_BUF_LEN) {initSize(size);}
+    template <typename U>
+    DspBuffer<T>(std::vector<U> data, DomainType dataDomain = TIME_DOMAIN) {initArray(VECTOR_TO_ARRAY(data), data.size(), dataDomain);}
+    template <typename U>
+    DspBuffer<T>(U *data, int dataLen, DomainType dataDomain = TIME_DOMAIN) {initArray(data, dataLen, dataDomain);}
 };
+
+template <class T>
+template <class U>
+void DspBuffer<T>::initArray(U *array, int arrayLen, DomainType dataDomain) {
+    buf = std::vector<T>(arrayLen);
+    for (int i=0; i<arrayLen; i++) {
+        buf[i] = (T) array[i];
+    }
+    domain = dataDomain;
+}
+
+#endif
--- test/DspBufferTest.cpp
+++ test/DspBufferTest.cpp
@@ -1,8 +1,34 @@
+#include <vector>
 #include "DspBuffer.h"
 #include "gtest/gtest.h"
 
 
-// Tests some trivial cases.
-TEST(TestTest, Trivial) {
-	EXPECT_EQ(1,1);
+TEST(DspBufferInit, Size) {
+	DspBuffer<int> buf(50);
+    
+    EXPECT_EQ(50, buf.buf.size());
+    EXPECT_EQ(TIME_DOMAIN, buf.domain);
+}
+
+TEST(DspBufferInit, Array) {
+    short expectedData[8] = {1, 3, 5, 7, 2, 4, 6, 8};
+	DspBuffer<int> buf(expectedData, 8, FREQUENCY_DOMAIN);
+    
+    EXPECT_EQ(8, buf.buf.size());
+    for (int i=0; i<buf.buf.size(); i++) {
+        EXPECT_EQ(expectedData[i], buf.buf[i]);
+    }
+    EXPECT_EQ(FREQUENCY_DOMAIN, buf.domain);
+}
+
+TEST(DspBufferInit, Vector) {
+    short array[8] = {1, 3, 5, 7, 2, 4, 6, 9};
+    std::vector<int> expectedData (array, array + sizeof(array) / sizeof(array[0]) );
+	DspBuffer<int> buf(expectedData);
+    
+    EXPECT_EQ(8, buf.buf.size());
+    for (int i=0; i<buf.buf.size(); i++) {
+        EXPECT_EQ(expectedData[i], buf.buf[i]);
+    }
+    EXPECT_EQ(TIME_DOMAIN, buf.domain);
 }
