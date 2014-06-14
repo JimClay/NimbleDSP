aa02e4c307c2d3f4c04d879e980423e2eb87efa9
Jim Clay <jim@whamcitylights.com>
2014-06-14 17:04:45 -0400

    Added a bunch of mathematical operators and comments to ComplexDspBuffer.

--- src/ComplexDspBuffer.h
+++ src/ComplexDspBuffer.h
@@ -6,6 +6,12 @@
 //
 //
 
+/**
+ * @file ComplexDspBuffer.h
+ *
+ * Definition of the template class ComplexDspBuffer.
+ */
+
 #ifndef SmartDsp_ComplexDspBuffer_h
 #define SmartDsp_ComplexDspBuffer_h
 
@@ -29,20 +35,132 @@ enum DomainType {TIME_DOMAIN, FREQUENCY_DOMAIN};
 template <class T>
 class ComplexDspBuffer : public DspBuffer< std::complex<T> > {
  public:
+    /**
+     * \brief Indicates whether the data in \ref buf is time domain data or frequency domain.
+     */
     DomainType domain;
     
-    // Constructors
-    ComplexDspBuffer<T>(void) : DspBuffer< std::complex<T> >() {domain = TIME_DOMAIN;}
-    ComplexDspBuffer<T>(unsigned size) : DspBuffer< std::complex<T> >(size) {domain = TIME_DOMAIN;}
+    /*****************************************************************************************
+                                        Constructors
+    *****************************************************************************************/
+    /**
+     * \brief Basic constructor.
+     *
+     * Sets \ref domain to SmartDsp::TIME_DOMAIN and calls DspBuffer<T>::DspBuffer(size, scratch).
+     * \param size Size of \ref buf.
+     * \param scratch Pointer to a scratch buffer.  The scratch buffer can be shared by multiple
+     *      objects (in fact, I recommend it), but if there are multiple threads then it should
+     *      be shared only by objects that are accessed by a single thread.  Objects in other
+     *      threads should have a separate scratch buffer.  If no scratch buffer is provided
+     *      then one will be created in methods that require one and destroyed when the method
+     *      returns.
+     */
+    ComplexDspBuffer<T>(unsigned size = DEFAULT_BUF_LEN, std::vector< std::complex<T> > *scratch = NULL) :
+            DspBuffer< std::complex<T> >(size, scratch) {domain = TIME_DOMAIN;}
+            
+    /**
+     * \brief Vector constructor.
+     *
+     * Sets buf equal to the input "data" parameter and sets the pointer to the scratch buffer,
+     *      if one is provided.  Also sets \ref domain to dataDomain.
+     * \param data Vector that \ref buf will be set equal to.
+     * \param scratch Pointer to a scratch buffer.  The scratch buffer can be shared by multiple
+     *      objects (in fact, I recommend it), but if there are multiple threads then it should
+     *      be shared only by objects that are accessed by a single thread.  Objects in other
+     *      threads should have a separate scratch buffer.  If no scratch buffer is provided
+     *      then one will be created in methods that require one and destroyed when the method
+     *      returns.
+     * \param dataDomain Indicates whether the data is time domain data or frequency domain.
+     *      Valid values are SmartDsp::TIME_DOMAIN and SmartDsp::FREQUENCY_DOMAIN.
+     */
     template <typename U>
     ComplexDspBuffer<T>(std::vector<U> data, DomainType dataDomain=TIME_DOMAIN) : DspBuffer< std::complex<T> >(data) {domain = dataDomain;}
+    
+    /**
+     * \brief Array constructor.
+     *
+     * Sets buf equal to the input "data" array and sets the pointer to the scratch buffer,
+     *      if one is provided.  Also sets \ref domain to dataDomain.
+     * \param data Array that \ref buf will be set equal to.
+     * \param dataLen Length of "data".
+     * \param scratch Pointer to a scratch buffer.  The scratch buffer can be shared by multiple
+     *      objects (in fact, I recommend it), but if there are multiple threads then it should
+     *      be shared only by objects that are accessed by a single thread.  Objects in other
+     *      threads should have a separate scratch buffer.  If no scratch buffer is provided
+     *      then one will be created in methods that require one and destroyed when the method
+     *      returns.
+     * \param dataDomain Indicates whether the data is time domain data or frequency domain.
+     *      Valid values are SmartDsp::TIME_DOMAIN and SmartDsp::FREQUENCY_DOMAIN.
+     */
     template <typename U>
     ComplexDspBuffer<T>(U *data, unsigned dataLen, DomainType dataDomain=TIME_DOMAIN) : DspBuffer< std::complex<T> >(data, dataLen) {domain = dataDomain;}
     
+    /**
+     * \brief Copy constructor.
+     */
     ComplexDspBuffer<T>(const ComplexDspBuffer<T>& other) {this->buf = other.buf; domain = other.domain;}
-    ComplexDspBuffer<T>& operator=(const ComplexDspBuffer<T>& rhs);
-    ComplexDspBuffer<T>& operator=(const DspBuffer< std::complex<T> >& rhs);
     
+    /*****************************************************************************************
+                                            Operators
+    *****************************************************************************************/
+    /**
+     * \brief Assignment operator.
+     */
+    ComplexDspBuffer<T>& operator=(const ComplexDspBuffer<T>& rhs);
+    ComplexDspBuffer<T>& operator=(const DspBuffer<T>& rhs);
+    
+    /**
+     * \brief Unary minus (negation) operator.
+     */
+    ComplexDspBuffer<T> & operator-();
+    
+    /**
+     * \brief Add Buffer/Assignment operator.
+     */
+    template <class U>
+    ComplexDspBuffer<T> & operator+=(const DspBuffer<U> &rhs);
+    
+    /**
+     * \brief Add Scalar/Assignment operator.
+     */
+    ComplexDspBuffer<T> & operator+=(const std::complex<T> &rhs);
+    
+    /**
+     * \brief Subtract Buffer/Assignment operator.
+     */
+    template <class U>
+    ComplexDspBuffer<T> & operator-=(const DspBuffer<U> &rhs);
+    
+    /**
+     * \brief Subtract Scalar/Assignment operator.
+     */
+    ComplexDspBuffer<T> & operator-=(const std::complex<T> &rhs);
+    
+    /**
+     * \brief Multiply Buffer/Assignment operator.
+     */
+    template <class U>
+    ComplexDspBuffer<T> & operator*=(const DspBuffer<U> &rhs);
+    
+    /**
+     * \brief Multiply Scalar/Assignment operator.
+     */
+    ComplexDspBuffer<T> & operator*=(const std::complex<T> &rhs);
+    
+    /**
+     * \brief Divide Buffer/Assignment operator.
+     */
+    template <class U>
+    ComplexDspBuffer<T> & operator/=(const DspBuffer<U> &rhs);
+    
+    /**
+     * \brief Divide Scalar/Assignment operator.
+     */
+    ComplexDspBuffer<T> & operator/=(const std::complex<T> &rhs);
+    
+    /*****************************************************************************************
+                                            Methods
+    *****************************************************************************************/
     // Methods
     ComplexDspBuffer<T> & pow(const std::complex<SMARTDSP_FLOAT_TYPE> & exponent);
     
@@ -69,11 +187,159 @@ ComplexDspBuffer<T>& ComplexDspBuffer<T>::operator=(const ComplexDspBuffer<T>& r
 }
 
 template <class T>
-ComplexDspBuffer<T>& ComplexDspBuffer<T>::operator=(const DspBuffer< std::complex<T> > & rhs)
+ComplexDspBuffer<T>& ComplexDspBuffer<T>::operator=(const DspBuffer<T> & rhs)
 {
-    this->buf = rhs.buf;
+    this->buf.resize(rhs.size());
+    for (unsigned i=0; i<this->size(); i++) {
+        this->buf[i] = std::complex<T>(rhs[i]);
+    }
     domain = TIME_DOMAIN;
     return *this;
+}
+
+template <class T>
+ComplexDspBuffer<T> & ComplexDspBuffer<T>::operator-()
+{
+    for (unsigned i=0; i<this->size(); i++) {
+        this->buf[i] = -(this->buf[i]);
+    }
+    return *this;
+}
+
+template <class T>
+template <class U>
+ComplexDspBuffer<T> & ComplexDspBuffer<T>::operator+=(const DspBuffer<U> &rhs)
+{
+    assert(this->size() == rhs.size());
+    for (unsigned i=0; i<this->size(); i++) {
+        this->buf[i] += rhs.buf[i];
+    }
+    return *this;
+}
+
+template <class T>
+ComplexDspBuffer<T> & ComplexDspBuffer<T>::operator+=(const std::complex<T> & rhs)
+{
+    for (unsigned i=0; i<this->size(); i++) {
+        this->buf[i] += rhs;
+    }
+    return *this;
+}
+
+template <class T, class U>
+inline ComplexDspBuffer<T> operator+(ComplexDspBuffer<T> lhs, const DspBuffer<U>& rhs)
+{
+    lhs += rhs;
+    return lhs;
+}
+
+template <class T>
+inline ComplexDspBuffer<T> operator+(ComplexDspBuffer<T> lhs, const std::complex<T> & rhs)
+{
+    lhs += rhs;
+    return lhs;
+}
+
+template <class T>
+template <class U>
+ComplexDspBuffer<T> & ComplexDspBuffer<T>::operator-=(const DspBuffer<U> &rhs)
+{
+    assert(this->size() == rhs.size());
+    for (unsigned i=0; i<this->size(); i++) {
+        this->buf[i] -= rhs.buf[i];
+    }
+    return *this;
+}
+
+template <class T>
+ComplexDspBuffer<T> & ComplexDspBuffer<T>::operator-=(const std::complex<T> &rhs)
+{
+    for (unsigned i=0; i<this->size(); i++) {
+        this->buf[i] -= rhs;
+    }
+    return *this;
+}
+
+template <class T, class U>
+inline ComplexDspBuffer<T> operator-(ComplexDspBuffer<T> lhs, const DspBuffer<U>& rhs)
+{
+    lhs -= rhs;
+    return lhs;
+}
+
+template <class T>
+inline ComplexDspBuffer<T> operator-(ComplexDspBuffer<T> lhs, const std::complex<T> & rhs)
+{
+    lhs -= rhs;
+    return lhs;
+}
+
+template <class T>
+template <class U>
+ComplexDspBuffer<T> & ComplexDspBuffer<T>::operator*=(const DspBuffer<U> &rhs)
+{
+    assert(this->size() == rhs.size());
+    for (unsigned i=0; i<this->size(); i++) {
+        this->buf[i] *= rhs.buf[i];
+    }
+    return *this;
+}
+
+template <class T>
+ComplexDspBuffer<T> & ComplexDspBuffer<T>::operator*=(const std::complex<T> &rhs)
+{
+    for (unsigned i=0; i<this->size(); i++) {
+        this->buf[i] *= rhs;
+    }
+    return *this;
+}
+
+template <class T, class U>
+inline ComplexDspBuffer<T> operator*(ComplexDspBuffer<T> lhs, const DspBuffer<U>& rhs)
+{
+    lhs *= rhs;
+    return lhs;
+}
+
+template <class T>
+inline ComplexDspBuffer<T> operator*(ComplexDspBuffer<T> lhs, const std::complex<T> & rhs)
+{
+    lhs *= rhs;
+    return lhs;
+}
+
+template <class T>
+template <class U>
+ComplexDspBuffer<T> & ComplexDspBuffer<T>::operator/=(const DspBuffer<U> &rhs)
+{
+    assert(this->size() == rhs.size());
+    for (unsigned i=0; i<this->size(); i++) {
+        this->buf[i] /= rhs.buf[i];
+    }
+    return *this;
+}
+
+template <class T>
+ComplexDspBuffer<T> & ComplexDspBuffer<T>::operator/=(const std::complex<T> &rhs)
+{
+    for (unsigned i=0; i<this->size(); i++) {
+        this->buf[i] /= rhs;
+    }
+    return *this;
+}
+
+template <class T, class U>
+inline ComplexDspBuffer<T> operator/(ComplexDspBuffer<T> lhs, const DspBuffer<U> & rhs)
+{
+    lhs /= rhs;
+    return lhs;
+}
+
+template <class T>
+inline ComplexDspBuffer<T> operator/(ComplexDspBuffer<T> lhs, const std::complex<T> & rhs)
+{
+    lhs /= rhs;
+    return lhs;
 }
 
 template <class T>
