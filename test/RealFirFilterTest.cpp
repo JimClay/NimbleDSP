e41a818d788c4eecb4093c1f929a1c8b75ab7d46
Jim Clay <jim@whamcitylights.com>
2014-06-21 23:32:54 -0400

    Added conv tests to RealFirFilterTest.

--- test/RealFirFilterTest.cpp
+++ test/RealFirFilterTest.cpp
@@ -40,3 +40,60 @@ TEST(RealFirFilter, ConvStream1) {
         EXPECT_EQ(expectedData3[i], buf[i]);
     }
 }
+
+
+TEST(RealFirFilter, ConvOneShot) {
+    double inputData[] = {1, 0, -1, -2, -3, -4, -5, -6, -7};
+    double inputData2[] = {-3, -4, -5, -6, -7, -8, -9};
+    int filterTaps[] = {1, 2, 3, 4, 5};
+    double expectedData[] = {1, 2, 2, 0, -5, -20, -35, -50, -65, -72, -70, -58, -35};
+    double expectedData2[] = {-3, -10, -22, -40, -65, -80, -95, -100, -94, -76, -45};
+    
+    unsigned numElements = sizeof(filterTaps)/sizeof(filterTaps[0]);
+    RealFirFilter<double> filter(filterTaps, numElements);
+    filter.filtOperation = ONE_SHOT_RETURN_ALL_RESULTS;
+
+    numElements = sizeof(inputData)/sizeof(inputData[0]);
+	DspBuffer<double> buf(inputData, numElements);
+    conv(buf, filter);
+    EXPECT_EQ(numElements + filter.size() - 1, buf.size());
+    for (unsigned i=0; i<buf.size(); i++) {
+        EXPECT_EQ(expectedData[i], buf[i]);
+    }
+    
+    numElements = sizeof(inputData2)/sizeof(inputData[0]);
+	buf = DspBuffer<double>(inputData2, numElements);
+    conv(buf, filter);
+    EXPECT_EQ(numElements + filter.size() - 1, buf.size());
+    for (unsigned i=0; i<buf.size(); i++) {
+        EXPECT_EQ(expectedData2[i], buf[i]);
+    }
+}
+
+TEST(RealFirFilter, ConvOneShotTrim) {
+    double inputData[] = {1, 0, -1, -2, -3, -4, -5, -6, -7};
+    double inputData2[] = {-3, -4, -5, -6, -7, -8, -9};
+    int filterTaps[] = {1, 2, 3, 4, 5};
+    double expectedData[] = {2, 0, -5, -20, -35, -50, -65, -72, -70};
+    double expectedData2[] = {-22, -40, -65, -80, -95, -100, -94};
+    
+    unsigned numElements = sizeof(filterTaps)/sizeof(filterTaps[0]);
+    RealFirFilter<double> filter(filterTaps, numElements);
+    filter.filtOperation = ONE_SHOT_TRIM_TAILS;
+
+    numElements = sizeof(inputData)/sizeof(inputData[0]);
+	DspBuffer<double> buf(inputData, numElements);
+    conv(buf, filter);
+    EXPECT_EQ(numElements, buf.size());
+    for (unsigned i=0; i<buf.size(); i++) {
+        EXPECT_EQ(expectedData[i], buf[i]);
+    }
+    
+    numElements = sizeof(inputData2)/sizeof(inputData[0]);
+	buf = DspBuffer<double>(inputData2, numElements);
+    conv(buf, filter);
+    EXPECT_EQ(numElements, buf.size());
+    for (unsigned i=0; i<buf.size(); i++) {
+        EXPECT_EQ(expectedData2[i], buf[i]);
+    }
+}
