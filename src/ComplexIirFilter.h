530ed63d3ccbf3178acf634007feae8e1274997a
Jim Clay <jim@whamcitylights.com>
2014-09-10 23:17:19 -0400

    Added ComplexIirFilter.

---
+++ src/ComplexIirFilter.h
@@ -0,0 +1,184 @@
+/*
+Copyright (c) 2014, James Clay
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+
+
+/**
+ * @file ComplexIirFilter.h
+ *
+ * Definition of the template class ComplexIirFilter.
+ */
+
+
+#ifndef NimbleDSP_ComplexIirFilter_h
+#define NimbleDSP_ComplexIirFilter_h
+
+#include <vector>
+#include "ComplexVector.h"
+
+
+namespace NimbleDSP {
+
+/**
+ * \brief Class for complex IIR filters.
+ */
+template <class T>
+class ComplexIirFilter {
+ protected:
+    std::vector< std::complex<T> > state;
+
+    /** 
+     * \brief Initializes numerator and denominator with the size and contents of "num" and "den".
+     *
+     * \param array Array to set vec equal to.
+     * \param arrayLen Number of elements in array.
+     */
+    template <class U>
+    void initArray(U *num, unsigned numLen, U *den, unsigned denLen);
+    
+ public:
+    std::vector< std::complex<T> > numerator;
+    std::vector< std::complex<T> > denominator;
+    
+    /*****************************************************************************************
+                                        Constructors
+    *****************************************************************************************/
+    /**
+     * \brief Basic constructor.
+     *
+     * Just sets the size of \ref buf and the pointer to the scratch buffer, if one is provided.
+     * \param size Size of \ref buf.
+     * \param scratch Pointer to a scratch buffer.  The scratch buffer can be shared by multiple
+     *      objects (in fact, I recommend it), but if there are multiple threads then it should
+     *      be shared only by objects that are accessed by a single thread.  Objects in other
+     *      threads should have a separate scratch buffer.  If no scratch buffer is provided
+     *      then one will be created in methods that require one and destroyed when the method
+     *      returns.
+     */
+    ComplexIirFilter<T>(unsigned order = 2) {numerator = std::vector< std::complex<T> >(order + 1);
+            denominator = std::vector< std::complex<T> >(order + 1); state = std::vector< std::complex<T> >(order + 1);}
+    
+    /**
+     * \brief Vector constructor.
+     *
+     * Sets buf equal to the input "data" parameter and sets the pointer to the scratch buffer,
+     *      if one is provided.
+     * \param data Vector that \ref buf will be set equal to.
+     * \param scratch Pointer to a scratch buffer.  The scratch buffer can be shared by multiple
+     *      objects (in fact, I recommend it), but if there are multiple threads then it should
+     *      be shared only by objects that are accessed by a single thread.  Objects in other
+     *      threads should have a separate scratch buffer.  If no scratch buffer is provided
+     *      then one will be created in methods that require one and destroyed when the method
+     *      returns.
+     */
+    template <typename U>
+    ComplexIirFilter<T>(std::vector< std::complex<U> > & num, std::vector< std::complex<U> > & den) {
+            initArray(VECTOR_TO_ARRAY(num), num.size(), VECTOR_TO_ARRAY(den), den.size());}
+    
+    /**
+     * \brief Array constructor.
+     *
+     * Sets vec equal to the input "data" array and sets the pointer to the scratch buffer,
+     *      if one is provided.
+     * \param data Array that \ref vec will be set equal to.
+     * \param dataLen Length of "data".
+     * \param scratch Pointer to a scratch buffer.  The scratch buffer can be shared by multiple
+     *      objects (in fact, I recommend it), but if there are multiple threads then it should
+     *      be shared only by objects that are accessed by a single thread.  Objects in other
+     *      threads should have a separate scratch buffer.  If no scratch buffer is provided
+     *      then one will be created in methods that require one and destroyed when the method
+     *      returns.
+     */
+    template <typename U>
+    ComplexIirFilter<T>(U *num, unsigned numLen, U *den, unsigned denLen) {initArray(num, numLen, den, denLen);}
+    
+    /*****************************************************************************************
+                                            Methods
+    *****************************************************************************************/
+    /**
+     * \brief Convolution method.
+     *
+     * \param data The vector that will be filtered.
+     * \param trimTails "False" tells the method to return the entire convolution, which is
+     *      the length of "data" plus the length of "this" (the filter) - 1.  "True" tells the
+     *      method to retain the size of "data" by trimming the tails at both ends of
+     *      the convolution.
+     * \return Reference to "data", which holds the result of the convolution.
+     */
+    template <class U>
+    ComplexVector<U> & filter(ComplexVector<U> & data);
+    
+};
+
+
+template <class T>
+template <class U>
+void ComplexIirFilter<T>::initArray(U *num, unsigned numLen, U *den, unsigned denLen) {
+    numerator = std::vector< std::complex<T> >(numLen);
+    for (unsigned i=0; i<numLen; i++) {
+        numerator[i] = (std::complex<T>) num[i];
+    }
+    
+    denominator = std::vector< std::complex<T> >(denLen);
+    for (unsigned i=0; i<denLen; i++) {
+        denominator[i] = (std::complex<T>) den[i];
+    }
+    
+    state = std::vector< std::complex<T> >(std::max(numLen, denLen));
+}
+
+template <class T>
+template <class U>
+ComplexVector<U> & ComplexIirFilter<T>::filter(ComplexVector<U> & data) {
+    unsigned resultIndex, i;
+    std::complex<U> newState0;
+    
+    for (resultIndex=0; resultIndex<data.size(); resultIndex++) {
+        newState0 = data[resultIndex];
+        
+        // Update the state
+        for (i=state.size()-1; i>=denominator.size(); i--) {
+            state[i] = state[i - 1];
+        }
+        // Update the state and apply the feedback
+        for (i=denominator.size()-1; i>0; i--) {
+            state[i] = state[i - 1];
+            newState0 -= denominator[i] * state[i];
+        }
+        state[0] = newState0;
+
+        // Calculate the output
+        data[resultIndex] = 0;
+        for (i=0; i<numerator.size(); i++) {
+            data[resultIndex] += numerator[i] * state[i];
+        }
+    }
+    return data;
+}
+
+template <class T, class U>
+ComplexVector<U> & filter(ComplexVector<U> & data, ComplexIirFilter<T> & filt) {
+    return filt.filter(data);
+}
+
+};
+
+#endif
---
+++ test/ComplexIirFilterTest.cpp
@@ -0,0 +1,146 @@
+/*
+Copyright (c) 2014, James Clay
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+
+#include "ComplexIirFilter.h"
+#include <vector>
+#include "gtest/gtest.h"
+
+using namespace NimbleDSP;
+
+extern bool ComplexEqual(std::complex<double> float1, std::complex<double> float2);
+
+TEST(ComplexIirFilter, ConstructorSize) {
+    ComplexIirFilter<double> filt(5);
+    
+    EXPECT_EQ(6, filt.numerator.size());
+    EXPECT_EQ(6, filt.denominator.size());
+}
+
+TEST(ComplexIirFilter, ConstructorVector) {
+    std::complex<double> den[] = {1, 2, 3, 4, 5, 6, 7, 8};
+    std::complex<double> num[] = {1, 2, 3};
+    std::vector< std::complex<double> > denominator(den, den + sizeof(den) / sizeof(den[0]));
+    std::vector< std::complex<double> > numerator(num, num + sizeof(num) / sizeof(num[0]));
+    
+    ComplexIirFilter<double> filt(numerator, denominator);
+    
+    EXPECT_EQ(8, filt.denominator.size());
+    for (unsigned i=0; i<filt.denominator.size(); i++) {
+        EXPECT_EQ(den[i], filt.denominator[i]);
+    }
+    
+    EXPECT_EQ(3, filt.numerator.size());
+    for (unsigned i=0; i<filt.numerator.size(); i++) {
+        EXPECT_EQ(num[i], filt.numerator[i]);
+    }
+}
+
+TEST(ComplexIirFilter, ConstructorArray) {
+    std::complex<double> den[] = {1, 2, 3, 4, 5, 6, 7, 8};
+    std::complex<double> num[] = {1, 2, 3};
+    
+    NimbleDSP::ComplexIirFilter<double> filt(num, sizeof(num) / sizeof(num[0]), den, sizeof(den) / sizeof(den[0]));
+    
+    EXPECT_EQ(8, filt.denominator.size());
+    for (unsigned i=0; i<filt.denominator.size(); i++) {
+        EXPECT_EQ(den[i], filt.denominator[i]);
+    }
+    
+    EXPECT_EQ(3, filt.numerator.size());
+    for (unsigned i=0; i<filt.numerator.size(); i++) {
+        EXPECT_EQ(num[i], filt.numerator[i]);
+    }
+}
+
+TEST(ComplexIirFilter, Filter1) {
+    std::complex<double> den[] = {1, .1};
+    std::complex<double> num[] = {1, .001};
+    std::complex<double> inputData[] = {1, 0, -1, -2, -3, -4, -5, -6, -7};
+    std::complex<double> expectedData[] = {1.00000000, -0.09900000, -0.99010000, -1.90199000, -2.81180100, -3.72181990, -4.63181801, -5.54181820, -6.45181818};
+    unsigned numElements = sizeof(inputData)/sizeof(inputData[0]);
+	NimbleDSP::ComplexVector<double> buf(inputData, numElements);
+    NimbleDSP::ComplexVector<double> input = buf;
+    
+    ComplexIirFilter<double> filt(num, sizeof(num) / sizeof(num[0]), den, sizeof(den) / sizeof(den[0]));
+    
+    filter(buf, filt);
+    EXPECT_EQ(input.size(), buf.size());
+    for (unsigned i=0; i<buf.size(); i++) {
+        EXPECT_TRUE(ComplexEqual(expectedData[i], buf[i]));
+    }
+}
+
+TEST(ComplexIirFilter, Filter2) {
+    std::complex<double> den[] = {1, .2, .1, .05, .02};
+    std::complex<double> num[] = {1, .05, .04, .03, .01};
+    std::complex<double> inputData[] = {1, 0, -1, -2, -3, -4, -5, -6, -7};
+    std::complex<double> expectedData[] = {1.00000000, -0.15000000, -1.03000000, -1.84900000, -2.66970000, -3.48666000, -4.31264800, -5.13833940, -5.96334032};
+    unsigned numElements = sizeof(inputData)/sizeof(inputData[0]);
+	NimbleDSP::ComplexVector<double> buf(inputData, numElements);
+    NimbleDSP::ComplexVector<double> input = buf;
+    
+    ComplexIirFilter<double> filt(num, sizeof(num) / sizeof(num[0]), den, sizeof(den) / sizeof(den[0]));
+    
+    filter(buf, filt);
+    EXPECT_EQ(input.size(), buf.size());
+    for (unsigned i=0; i<buf.size(); i++) {
+        EXPECT_TRUE(ComplexEqual(expectedData[i], buf[i]));
+    }
+}
+
+TEST(ComplexIirFilter, Filter3) {
+    std::complex<double> den[] = {1, .2, .1, .05, .02};
+    std::complex<double> num[] = {1, .05, .04};
+    std::complex<double> inputData[] = {1, 0, -1, -2, -3, -4, -5, -6, -7};
+    std::complex<double> expectedData[] = {1.00000000, -0.15000000, -1.03000000, -1.87900000, -2.67370000, -3.45286000, -4.24750800, -5.04394740, -5.84034272};
+    unsigned numElements = sizeof(inputData)/sizeof(inputData[0]);
+	NimbleDSP::ComplexVector<double> buf(inputData, numElements);
+    NimbleDSP::ComplexVector<double> input = buf;
+    
+    ComplexIirFilter<double> filt(num, sizeof(num) / sizeof(num[0]), den, sizeof(den) / sizeof(den[0]));
+    
+    filter(buf, filt);
+    EXPECT_EQ(input.size(), buf.size());
+    for (unsigned i=0; i<buf.size(); i++) {
+        EXPECT_TRUE(ComplexEqual(expectedData[i], buf[i]));
+    }
+}
+
+TEST(ComplexIirFilter, Filter4) {
+    std::complex<double> den[] = {1, .2};
+    std::complex<double> num[] = {1, .05, .04, .03, .01};
+    std::complex<double> inputData[] = {1, 0, -1, -2, -3, -4, -5, -6, -7};
+    std::complex<double> expectedData[] = {1.00000000, -0.15000000, -0.93000000, -1.83400000, -2.76320000, -3.70736000, -4.64852800, -5.59029440, -6.53194112};
+    unsigned numElements = sizeof(inputData)/sizeof(inputData[0]);
+	NimbleDSP::ComplexVector<double> buf(inputData, numElements);
+    NimbleDSP::ComplexVector<double> input = buf;
+    
+    ComplexIirFilter<double> filt(num, sizeof(num) / sizeof(num[0]), den, sizeof(den) / sizeof(den[0]));
+    
+    filter(buf, filt);
+    EXPECT_EQ(input.size(), buf.size());
+    for (unsigned i=0; i<buf.size(); i++) {
+        EXPECT_TRUE(ComplexEqual(expectedData[i], buf[i]));
+    }
+}
+
+
